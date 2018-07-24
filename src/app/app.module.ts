f4ad4a14d54c01c420a320d979796168323248a3
Walter Loburak <you@example.comvldlbrk@gmail.com>
2018-07-24 16:01:33 -0500

    - top bar completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    MainNavComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/topbar/topbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { faFacebookF, faTwitter, faLinkedinIn, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeOpen, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+
+  fb = faFacebookF;
+  tw = faTwitter;
+  lin = faLinkedinIn;
+  gp = faGooglePlusG;
+  tmail = faEnvelopeOpen;
+  phone = faPhone;
+
+  socialLinks: = {
+    fb: 'https://www.facebook.com/walter.loburak',
+    tw: 'https://twitter.com/vldlbrk',
+    lin: 'https://www.linkedin.com/in/lpq66',
+    gp: 'https://plus.google.com/u/0/111743041512359692263'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
