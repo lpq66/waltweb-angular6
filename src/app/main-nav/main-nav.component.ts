8e4437a84e51d8ba572445072baa65209c95ef52
Walter Loburak <you@example.comvldlbrk@gmail.com>
2018-07-24 16:51:30 -0500

    - finish main-nav component(visual)

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
+  const navLinks = [{
+    anchor: '#about', name: 'About me'
+  }, {
+    anchor: '#wcid', name: 'What can I do for you'
+  }, {
+    anchor: '#portfolio', name: 'Portfolio'
+  }, {
+    anchor: '#testimonials', name: 'Testimonials'
+  }, {
+    anchor: '#contact', name: 'Contact'
+  }, {
+    anchor: '#pricing', name: 'Pricing'
+  }]
+
   constructor() { }
 
   ngOnInit() {
