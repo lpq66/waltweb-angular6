2d20325b03efb6686e461c9e2c266a388c9aa1ad
Walter Loburak <you@example.comvldlbrk@gmail.com>
2018-08-01 18:26:15 -0500

    - add mobile navigation

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { PageScrollConfig } from 'ngx-page-scroll';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  styleUrls: ['./main-nav.component.scss'],
+  animations: [
+    trigger('openNav', [
+      state('show', style({
+        transform: "translateX(0)"
+      })),
+      state('hide', style({
+        transform: "translateX(-100%)"
+      })),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in'))
+    ]),
+    trigger('btn', [
+      state('open', style({
+        transform: "rotate(20deg)"
+      })),
+      state('close', style({
+        transform: "rotate(0deg)"
+      })),
+      transition('open => close', animate('300ms ease-out')),
+      transition('close => open', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class MainNavComponent implements OnInit {
 
@@ -32,17 +61,17 @@ export class MainNavComponent implements OnInit {
   scrollSpy: any;
 
   navLinks = [{
-    anchor: '#about', name: 'About me'
+    anchor: '#about', name: 'About me', id: 1
   }, {
-    anchor: '#wcid', name: 'What can I do for you'
+    anchor: '#wcid', name: 'What can I do for you', id: 2
   }, {
-    anchor: '#portfolio', name: 'Portfolio'
+    anchor: '#portfolio', name: 'Portfolio', id: 3
   }, {
-    anchor: '#testimonials', name: 'Testimonials'
+    anchor: '#testimonials', name: 'Testimonials', id: 4
   }, {
-    anchor: '#contact', name: 'Contact'
+    anchor: '#contact', name: 'Contact', id: 5
   }, {
-    anchor: '#pricing', name: 'Pricing'
+    anchor: '#pricing', name: 'Pricing', id: 6
   }]
 
   getDistance = () => {
@@ -50,17 +79,41 @@ export class MainNavComponent implements OnInit {
     return this.top;
   }
 
+  show = false
+  open = false
+
+  get openName() {
+    return this.open ? 'open' : 'close'
+  }
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
+
+  toggle() {
+    this.show = !this.show
+  }
+
+  btnToggle() {
+    this.open = !this.open
+  }
+
   @HostListener('window:scroll', ['$event'])
   onScroll($event): void {
     this.stickyNav = this.elRef.nativeElement.querySelector('.main-nav');
+    const mobileNav = this.elRef.nativeElement.querySelector('.main-nav--mobile')
     this.offset = window.pageYOffset;
     this.distance = this.getDistance() - this.offset;
     if (this.distance <= 0 && !this.stuck) {
       this.stickyNav.classList.add('sticky');
+      mobileNav.classList.add('sticky');
       this.stuck = true;
     } else if (this.stuck && this.offset <= this.stickPoint) {
       this.stickyNav.classList.remove('sticky');
       this.stuck = false;
+    }
+
+    if (this.stuck && this.offset <= 50) {
+      mobileNav.classList.remove('sticky');
     }
   }
 
